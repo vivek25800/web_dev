391bee6a459e06a7f35fcdcfe1459a46cdd83a38
Vivek Gupta <vg4213274@gmail.com>
2026-03-04 18:17:45 +0530

    updated

--- JavaScript/Function Call stack and Hoisting/callStackAndHoistingConcept.js
+++ JavaScript/Function Call stack and Hoisting/callStackAndHoistingConcept.js
@@ -62,6 +62,88 @@
 // Function Call Satck in Javascript - 
 // It keeps track of function calls in the order they are executed.
 // It determine which function is currently running and what runs next.
+// The call stack works based on the last-in-first-out (LIFO) priciple.
+// Example - 
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function average(num1, num2) {
+//     return add(num1, num2) / 2;
+// }
+
+// let result = average(10, 20);
+// console.log("Average is : ", result);
+
+// Functions - First class citizens
+// Functions are treated like any other varaible.
+// That means functions can:
+// 1. Be assigned to a variable
+// 2. Be passed as an argument to another function
+// 3. Be returned from another function
+// 4. Be stored inside objects or arrays
+
+// Function Assigned to a Variable
+// Example -
+// const greet = function() {
+//     console.log("Hello World");
+// }
+// greet();
+
+// Function Passed as Argument (Callback)
+// Example - 
+// function sayHello() {
+//     console.log("Hello");
+// }
+// function executeFunction(fnc) {
+//     fnc();
+// }
+// executeFunction(sayHello);
+
+// Function Returned From Another Function
+//Example - 
+function outer() {
+    return function inner() {
+        console.log("Inside inner function!");
+    };
+}
+let result = outer();
+result();
+
+// Function Stored in Object
+// Example - 
+// const person = {
+//     name: "Vivek",
+//     age: 25,
+//     greet: function() {
+//         console.log("Hello Duniya");
+//     }
+// }
+// person.greet(); // Print - Hello Duniya
+
+// Functions with Data Structures (Array)
+// Example - 
+let arr = [
+    function(a, b) {
+        return a + b;
+    },
+    function(a, b) {
+        return a - b;
+    },
+    function(a, b) {
+        return a * b;
+    }
+];
+
+let first = arr[0];
+let ans = first(10, 10); // print 20
+console.log(ans);
+
+
+
+
+
 
 
 
