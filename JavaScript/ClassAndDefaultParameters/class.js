91984db8cdd05ffabe65e253ec20e7a86423a02e
Vivek Gupta <vg4213274@gmail.com>
2026-03-06 22:27:23 +0530

    update

--- JavaScript/ClassAndDefaultParameters/class.js
+++ JavaScript/ClassAndDefaultParameters/class.js
@@ -3,30 +3,121 @@
 // Instead of creating many similar objects manually, we use a class to define their structure once.
 
 // Example - 
+// class Human {
+//     //Properties
+//     age = 25;
+//     // make it private
+//     #weight = 80;
+//     height = 180;
 
-class Human {
-    //Properties
-    age = 25;
-    // make it private
-    #weight = 80;
-    height = 180;
+//     // Behaviour 
+//     walking() {
+//         console.log("I am walking");
+//         console.log(this.#weight); // print 80
+//     }
 
-    // Behaviour 
-    walking() {
-        console.log("I am walking");
-        console.log(this.#weight); // print 80
-    }
+//     running() {
+//         console.log("I am running");
+//     }
+// }
+// // Creating object 
+// const obj = new Human();
+// console.log(obj.age);
+// console.log(obj.height);
+// obj.walking();
+// console.log(obj.weight); // print 'undefined'
 
-    running() {
-        console.log("I am running");
+// Constructor - Special method used to initialize object values.
+// Example 2 - 
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log("Hello my name is", this.name);
+//         console.log("I am ", this.age, " years old");
+//     }
+// }
+// const obj1 = new Person("Vivek", 25);
+// obj1.greet();
+
+// Class with multiple Methods
+// Example - 
+// class Car {
+//     constructor(brand, model) {
+//         this.brand = brand;
+//         this.model = model;
+//     }
+
+//     start() {
+//         console.log("Car is started");
+//     }   
+    
+//     stop() {
+//         console.log("Car is stop");
+//     }
+
+// }
+// const car1 = new Car("BMW", "X7");
+// car1.start();
+// car1.stop();
+
+// Getter and Setter in Class
+// these mathods use for access the private data to outside the class.
+//Example - 
+// class Student {
+//     age = 25;
+//     #weight = 80;
+//     height = 180;
+
+//     // Getter function to get data
+//     get fetchWeight() {
+//         return this.#weight;
+//     }
+
+//     // Setter function to update the data
+//     set modifyHeight(value) {
+//         this.height = value;
+//     }
+// }
+// const obj1 = new Student();
+// // Use get method
+// let age2 = obj1.fetchWeight;
+// console.log("My age: ", age2);
+
+// Contructor method - 
+// A constructor is a special method inside a class that runs automatically when a new object is created.
+// It is mainly used to set initial values for object properties.
+// Syntax of Contructor
+// class ClassName {
+//     constructor(parameters) {
+//         // initialize properties
+//     }
+// }
+
+// Example - Basic constructor
+class Person {
+    name;
+    age;
+    // Creating Constructor
+    constructor(newName, newAge) {
+        this.name = newName;
+        this.age = newAge;
     }
 }
-// Creating object 
-const obj = new Human();
-console.log(obj.age);
-console.log(obj.height);
-obj.walking();
-console.log(obj.weight); // print 'undefined'
+const obj = new Person("Vivek", 25);
+console.log("My name is", obj.name);
+console.log("My age", obj.age);
+
+
+
+
+
+
+
+
 
 
 
