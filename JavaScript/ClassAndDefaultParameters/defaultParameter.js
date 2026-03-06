7987f2e45c0d2e9889665b132e80fd9fcea543a8
Vivek Gupta <vg4213274@gmail.com>
2026-03-07 03:34:41 +0530

    update

--- JavaScript/ClassAndDefaultParameters/defaultParameter.js
+++ JavaScript/ClassAndDefaultParameters/defaultParameter.js
@@ -1,9 +1,9 @@
 // Default parameters - 
 // Default parameters allow you to assign a defalut value to a function parameter if no value (or undefined) is passed when the function is called.
 // Basic Syntax - 
-function functionName(parameter = defaultValue) {
-    // code 
-}
+// function functionName(parameter = defaultValue) {
+//     // code 
+// }
 
 // Example1 - Simple default parameter
 function greet(name = "Vivek") {
@@ -18,7 +18,20 @@ function add(num1 = 10, num2 = 20) {
 }
 console.log(add());
 
+// Default Parameter with Expression
+function multiply(a, b = a * 5) {
+    console.log(a, b);
+}
+multiply(20); // print 20, 100 (20 * 5)
 
+// Default Parameter with Function
+function getValue() {
+    return "Hello Duniya";
+}
+function show(value = getValue()) {
+    console.log(value);
+}
+show();
 
 
 
