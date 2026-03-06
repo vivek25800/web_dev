3f6eb529bdcb4ea2fc10dc934821d26c9405a640
Vivek Gupta <vg4213274@gmail.com>
2026-03-07 04:31:36 +0530

    all code updated

--- JavaScript/ClassAndDefaultParameters/class.js
+++ JavaScript/ClassAndDefaultParameters/class.js
@@ -1,4 +1,4 @@
-// Classes are atempleate for creating objects.
+// Classes are a templeate for creating objects.
 // Classes in JS are built on prototypes but also have some syntax and semantics that are uniques to classes.
 // Instead of creating many similar objects manually, we use a class to define their structure once.
 
@@ -6,7 +6,7 @@
 // class Human {
 //     //Properties
 //     age = 25;
-//     // make it private
+//     // make it private using #
 //     #weight = 80;
 //     height = 180;
 
@@ -25,7 +25,7 @@
 // console.log(obj.age);
 // console.log(obj.height);
 // obj.walking();
-// console.log(obj.weight); // print 'undefined'
+// console.log(obj.weight); // print 'undefined' because its private
 
 // Constructor - Special method used to initialize object values.
 // Example 2 - 
@@ -65,8 +65,8 @@
 // car1.stop();
 
 // Getter and Setter in Class
-// these mathods use for access the private data to outside the class.
-//Example - 
+// These mathods use for access the private data to outside the class.
+// Example - 
 // class Student {
 //     age = 25;
 //     #weight = 80;
@@ -90,7 +90,7 @@
 // Contructor method - 
 // A constructor is a special method inside a class that runs automatically when a new object is created.
 // It is mainly used to set initial values for object properties.
-// Syntax of Contructor
+// Syntax of Contructor - 
 // class ClassName {
 //     constructor(parameters) {
 //         // initialize properties
@@ -98,38 +98,38 @@
 // }
 
 // Example - Basic constructor
-class Person {
-    name;
-    age;
-    // Creating Constructor
-    constructor(newName, newAge) {
-        this.name = newName;
-        this.age = newAge;
-    }
-}
-const obj = new Person("Vivek", 25);
-console.log("My name is", obj.name);
-console.log("My age", obj.age);
+// class Person {
+//     name;
+//     age;
+//     // Creating Constructor
+//     constructor(newName, newAge) {
+//         this.name = newName;
+//         this.age = newAge;
+//     }
+// }
+// const obj = new Person("Vivek", 25);
+// console.log("My name is", obj.name);
+// console.log("My age", obj.age);
 
 // Constructor + Method
 // Example - 
-class Car {
-    constructor(brand, model) {
-        this.brand = brand;
-        this.model = model;
-    }
+// class Car {
+//     constructor(brand, model) {
+//         this.brand = brand;
+//         this.model = model;
+//     }
 
-    carStart() {
-        console.log(this.brand + " car started");
-    }
+//     carStart() {
+//         console.log(this.brand + " car started");
+//     }
 
-    carStop() {
-        console.log(this.brand + " car stoped");
-    }
-}
-const object = new Car("BMW", "X7");
-object.carStart();
-object.carStop();
+//     carStop() {
+//         console.log(this.brand + " car stoped");
+//     }
+// }
+// const object = new Car("BMW", "X7");
+// object.carStart();
+// object.carStop();
 
 
 
--- JavaScript/ClassAndDefaultParameters/defaultParameter.js
+++ JavaScript/ClassAndDefaultParameters/defaultParameter.js
@@ -6,38 +6,65 @@
 // }
 
 // Example1 - Simple default parameter
-function greet(name = "Vivek") {
-    console.log("Hello " + name);
-} 
-greet();
-greet("Rohit");
+// function greet(name = "Vivek") {
+//     console.log("Hello " + name); // print 'Hello Vivek'
+// } 
+// greet();
+// greet("Rohit"); // print 'Hello Rohit'
 
 // Example2 - Multiple Default Parameters
-function add(num1 = 10, num2 = 20) {
-    return num1 + num2;
-}
-console.log(add());
+// function add(num1 = 10, num2 = 20, name = "Vivek") {
+//     console.log("Result: ", num1 + num2);
+//     console.log("Name is: ", name);
+// }
+// add(50, 45, "Rohit");
 
 // Default Parameter with Expression
-function multiply(a, b = a * 5) {
-    console.log(a, b);
-}
-multiply(20); // print 20, 100 (20 * 5)
+// function multiply(a, b = a * 5) {
+//     console.log(a, b);
+// }
+// multiply(20); // print 20, 100 (20 * 5)
 
 // Default Parameter with Function
-function getValue() {
-    return "Hello Duniya";
-}
-function show(value = getValue()) {
-    console.log(value);
-}
-show();
+// function getValue() {
+//     return "Hello Duniya";
+// }
+// function show(value = getValue()) {
+//     console.log(value);
+// }
+// show();
 
+// Passing Object in Parameter
+// function student(value = {age: 20, city: "Noida", state: "UP"}) {
+//     console.log(value); 
+//     console.log(value.city); // print Noida
+// }
+// student();
 
+// Passing Array In Parameter
+// function solve(arr = [10, 20, 30, 40]) {
+//     console.log(arr);
+//     console.log(arr[2]); // print 30
+// }
+// solve();
 
+// // Default parameter with 'null' and 'undefined'
+// function example(value = "Vivek") {
+//     console.log("Hello", value);
+// }
+// // passing null value
+// example(null); // in this print 'null'
+// // passing undefined
+// example(undefined); // in this print 'Hello Vivek' (same value)
 
-
-
+// // Passing a function in default paramter
+// function getAge() {
+//     return 25;
+// } 
+// function utility(name, age = getAge()) {
+//     console.log(name, " ", age);
+// }
+// utility("Vivek");
 
 
 
