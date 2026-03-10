a8052050209d827a0c2c067c1f52d9c508b1d330
Vivek Gupta <vg4213274@gmail.com>
2026-03-10 22:01:54 +0530

    all code update

--- JavaScript/Common In Built Objects/concept.js
+++ JavaScript/Common In Built Objects/concept.js
@@ -3,36 +3,45 @@ console.log("Hello Duniya");
 // Math function in JS
 
 // Find the value of PI using Math function
-console.log(Math.PI); // Print - 3.141592653589793
+// console.log(Math.PI); // Print - 3.141592653589793
 
-// Find the maximum number
-console.log(Math.max(34, 45, 567, 43, 56, 67)); // print 567
+// // Find the maximum number
+// console.log(Math.max(34, 45, 567, 43, 56, 67)); // print 567
 
-// Find the minimum number
-console.log(Math.min(56, 34, 58, 20, 48, 3)); // print 3
+// // Find the minimum number
+// console.log(Math.min(56, 34, 58, 20, 48, 3)); // print 3
 
-// Rounding Functions - 
+// // Rounding Functions - 
 
-// Round to nearest integer
-console.log(Math.round(2.6)); // print 3
-console.log(Math.round(2.4)); // print 2
+// // Round to nearest integer
+// console.log(Math.round(2.6)); // print 3
+// console.log(Math.round(2.4)); // print 2
 
-// Rounds down.
-console.log(Math.floor(4.5)); // print 4
-console.log(Math.floor(5.9)); // print 5
+// // Rounds down.
+// console.log(Math.floor(4.5)); // print 4
+// console.log(Math.floor(5.9)); // print 5
 
-// Rounds Up.
-console.log(Math.ceil(2.1)); // print 3
-console.log(Math.ceil(2.9)); // print 3 
+// // Rounds Up.
+// console.log(Math.ceil(2.1)); // print 3
+// console.log(Math.ceil(2.9)); // print 3 
 
-// Removes decimal part
-console.log(Math.trunc(4.8)); // print 4
-console.log(Math.trunc(6.9)); // print 6
+// // Removes decimal part
+// console.log(Math.trunc(4.8)); // print 4
+// console.log(Math.trunc(6.9)); // print 6
 
+// Power and Root Functions
 
+// Power of number
+console.log(Math.pow(4,5)); // print 1024
+console.log(Math.pow(2,3)); // print 8
 
+// Square root
+console.log(Math.sqrt(16)); // print 4
+console.log(Math.sqrt(100)); // print 10
 
-
+// Cube root
+console.log(Math.cbrt(27)); // print 3
+console.log(Math.cbrt(125)); // print 5
 
 
 
