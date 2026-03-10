333242b10782c4370b2518b411c759e21350cf75
Vivek Gupta <vg4213274@gmail.com>
2026-03-10 21:53:26 +0530

    code update

---
+++ JavaScript/Common In Built Objects/concept.js
@@ -0,0 +1,42 @@
+console.log("Hello Duniya");
+
+// Math function in JS
+
+// Find the value of PI using Math function
+console.log(Math.PI); // Print - 3.141592653589793
+
+// Find the maximum number
+console.log(Math.max(34, 45, 567, 43, 56, 67)); // print 567
+
+// Find the minimum number
+console.log(Math.min(56, 34, 58, 20, 48, 3)); // print 3
+
+// Rounding Functions - 
+
+// Round to nearest integer
+console.log(Math.round(2.6)); // print 3
+console.log(Math.round(2.4)); // print 2
+
+// Rounds down.
+console.log(Math.floor(4.5)); // print 4
+console.log(Math.floor(5.9)); // print 5
+
+// Rounds Up.
+console.log(Math.ceil(2.1)); // print 3
+console.log(Math.ceil(2.9)); // print 3 
+
+// Removes decimal part
+console.log(Math.trunc(4.8)); // print 4
+console.log(Math.trunc(6.9)); // print 6
+
+
+
+
+
+
+
+
+
+
+
+
