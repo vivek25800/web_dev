b69fa4e89bcdb3ca30fda8e97cb617cf8baa6a62
Vivek Gupta <vg4213274@gmail.com>
2026-03-18 03:45:29 +0530

    commit

---
+++ JavaScript/Object Cloning Nad Garbage Collector/garbageCollector.js
@@ -0,0 +1,20 @@
+
+// Garbage Collection - 
+// Garbage collection is the process of automatically freeing memory that is no longer used.
+
+// Javascript uses automatic memory management
+
+// An object is kept in memory as long as it is reachable.
+// Example - 
+let user = { name: "Vivek" };
+user = null;
+console.log(user);
+// Now the object becomes unreachable -> eligible for gafrbage collection
+
+
+
+
+
+
+
+
