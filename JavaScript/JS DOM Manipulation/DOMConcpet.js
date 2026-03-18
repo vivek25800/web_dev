5236748d27c32a3464cca7bbdfcb5bbe72f478f9
Vivek Gupta <vg4213274@gmail.com>
2026-03-19 04:50:19 +0530

    code update today

---
+++ JavaScript/JS DOM Manipulation/DOMConcpet.js
@@ -0,0 +1,19 @@
+
+// DOM Manipulation in JavaScript
+
+// DOM is tree structure of your HTML page that JavaScript can read and modify.
+
+// 1. Access Elements
+// First, you need to grab elements from the page.
+// Example -
+
+const el = document.getElementById('title');
+// console.log(el);
+
+
+
+
+
+
+
+
