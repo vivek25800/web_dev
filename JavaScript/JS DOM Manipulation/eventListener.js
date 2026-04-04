644b6e47485da7ebd058e5da73bad68f58c5b915
Vivek Gupta <vg4213274@gmail.com>
2026-04-04 12:27:37 +0530

    updated code

--- JavaScript/JS DOM Manipulation/eventListener.js
+++ JavaScript/JS DOM Manipulation/eventListener.js
@@ -20,3 +20,50 @@ function changeText() {
     document.getElementById('myText').textContent = "Now i am becoming the destroyer of the world";
 }
 btn2.addEventListener('click', changeText);
+
+// For removing an event listener
+// element.removeEventListener(type, listener, options)
+// element.removeEventListener(type, listener, useCapture)
+
+// Example: Removing the click event listener from the button
+// btn2.removeEventListener('click', changeText); // This will not chnage the text when button is clicked
+
+
+// Phases in Javascript Events
+// 1. Capturing Phase: The event starts from the window and goes down to the target element.
+// 2. Target Phase: The event reaches the target element and triggers any event listeners attached to it.
+// 3. Bubbling Phase: After the target phase, the event bubbles back up from the target element to the window, triggering any event listeners attached to the ancestors of the target element.
+
+let outer = document.getElementById('outer');
+let middle = document.getElementById('middle');
+let inner = document.getElementById('inner');
+
+// Adding event listeners to the divs
+outer.addEventListener('click', function() {
+    console.log('Capturing Phase: Outer div');
+}, true); // true for capturing phase
+
+middle.addEventListener('click', function() {
+    console.log('Capturing Phase: Middle div');
+}, true); // true for capturing phase
+
+inner.addEventListener('click', function() {
+    console.log('Target Phase: Inner div');
+});
+
+outer.addEventListener('click', function() {
+    console.log('Bubbling Phase: Outer div');
+});
+
+middle.addEventListener('click', function() {
+    console.log('Bubbling Phase: Middle div');
+});
+
+inner.addEventListener('click', function() {
+    console.log('Bubbling Phase: Inner div');
+});
+
+
+
+
+
